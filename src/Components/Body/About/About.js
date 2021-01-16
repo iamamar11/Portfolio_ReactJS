dd913aa871720ffb7964feb82e91c1e7d535c884
iamamar11 <37834168+iamamar11@users.noreply.github.com>
2021-01-16 16:08:16 -0500

    fixed all minor detials

--- src/Components/Body/About/About.js
+++ src/Components/Body/About/About.js
@@ -12,12 +12,13 @@ const About = () => {
                 <a href='mailto: iamamarjit95@gmail.com' style={{color : "#BD5D38"}} title='Send email to Amarjit'> iamamarjit95@gmail.com </a>&middot;
                 4000 Avenue Dupuis, Montreal, H3T-1E8
             </span>
-            <h4 className='description'>Enthusiastic and Detail-oriented Web Developer, specializing in Front end development with React library. Good practical and Supportive skill to work as a team player. Looking forward to join reputative company to start my professional career as a web developer. Eager to learn more about web development technologies.  </h4>
+            <h4 className='description'>Enthusiastic and Detail-oriented Web Developer, specializing in Front end development with React library. Good practical and Supportive skill to work as a team player. Eager to learn more about web development technologies. Looking forward to join reputative company to start my professional career as a web developer.</h4>
             <SocialList />
             <div className='downloadSection'>
                 <a href={resume} download ><button className='downloadResume'>Download Resume</button></a>
             </div>
         </div>
+        
     )
 }
 
